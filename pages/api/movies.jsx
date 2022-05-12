7d87bb9ee1cc4666f065add923c5cc94d539a34e
Falller <marlon.emanutec@gmail.com>
2022-05-11 22:13:27 -0300

    post feito, porém não pronto

--- pages/api/movies.jsx
+++ pages/api/movies.jsx
@@ -1,14 +1,45 @@
 import connectToDatabase from "../../lib/mongodb";
 
+/*
 export default async function (req, res) {
     const db = await connectToDatabase();
 
     if (req.method == "GET") {
         const movies = await db.collection("movies")
-            .find({year: 2010})
+            .find({year: 2010}) // essas coisas da pra tratar na outra página
             .sort({ title: 1 })
             .toArray();
 
         res.json(movies);
     }
 }
+*/
+export default async function handler(req, res) {
+    
+    const db = await connectToDatabase();
+    switch (req.method) {
+        
+      case "POST":
+        let bodyObject = JSON.parse(req.body);
+        let newPost = await db.collection("movies").insertOne(bodyObject);
+        res.json(newPost);//.ops[0]);
+        console.log("BodyObject", bodyObject)
+        console.log("está no post2", req.body)
+        console.log("newPost",newPost)
+        break;
+
+      case "GET":
+        const movies = await db.collection("movies")
+            .find({year: 2010}) // essas coisas da pra tratar na outra página
+            .sort({ title: 1 })
+            .toArray();
+
+        res.json(movies);
+       // const posts = await db.collection("posts").find({}).toArray();
+       // res.json({ status: 200, data: posts });
+        break;
+
+       // case "DELETE":
+        
+    }
+  }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 //import Head from 'next/head'
 //import clientPromise from '../lib/mongodb'
 
-import { useEffect, useState } from "react";
 
 const URL = process.env.BASE_URL;
 
@@ -20,7 +19,33 @@ export default function Home({movies: moviesArray}) {
 }
 
 export async function getServerSideProps(context) {
-  const response = await fetch(URL + "api/movies");
+  //POST
+  let res = await fetch(URL + "api/movies", //resposta post
+  {
+      method: "POST",
+      body: JSON.stringify(
+        {
+        plot:"Three men hammer on an anvil and pass a bottle of beer around.",
+        title:"00001 TESTE SQUAD AMARELA",
+        fullplot:"A stationary camera looks at a large anvil with a blacksmith behind it...",
+        rated:"UNRATED",
+        lastupdated:"2015-08-26 00:03:50.133000000",
+        year:2010,
+        type:"movie",
+    }),
+  
+  });
+  res = await res.json();
+  // //DELETE
+  // let resDelete = await fetch(URL + "api/movies", 
+  // {
+  //   method: "DELETE"
+  // }
+
+  //GET
+  const response = await fetch(URL + "api/movies", {
+    method: "GET"
+  });
   const movies = await response.json();
 
   return {
